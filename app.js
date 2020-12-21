3a70b514091e59d0d4e769c32fe15805636a6716
Benedikt89 <constantine.kostka@gmail.com>
2020-12-21 21:35:35 +0300

    setting up heroku - partially

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const cors = require("cors");
 import createError from 'http-errors';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import mongoose from "mongoose";
 
 import indexRouter from './routes/index';
 import todosRouter from './routes/todos';
 
-mongoose.connect('mongodb://localhost/todolist', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.Promise = global.Promise;
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function () {
-  console.log('DB-CONNECTED')
-});
-
-var app = express();
 
 const corsOptions = {
   origin: true,
@@ -28,6 +23,37 @@ app.use(cors(corsOptions));
 
 //Configure
 app.disable("x-powered-by");
+
+
+//-----------------------------------------
+//Mongoose Settings
+//-----------------------------------------
+const mongoose = require("mongoose");
+console.log('mongoose staff');
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+//console.log(db);
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+  console.log('DB-CONNECTED')
+});
+
+app.use((req, res, next) => {
+  console.log('use for mongoose callback');
+  if (mongoose.connection.readyState) {
+    console.log('readyState');
+    next();
+  } else {
+    console.log('else connection mongoose');
+    require('./mongo')().then(() => {
+      next();
+    });
+  }
+});
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -47,7 +73,27 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
+// ----------------------------------------
+// Server
+// ----------------------------------------
+const port = process.env.PORT || process.argv[2] || 3000;
+const host = "localhost";
+
+let args;
+process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
+
+args.push(() => {
+  console.log(`Listening: http://${host}:${port}\n`);
+});
+
+if (require.main === module) {
+  app.listen.apply(app, args);
+}
+
+// ----------------------------------------
+// Error Handling
+// ----------------------------------------
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -58,11 +104,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.APP_PORT || 8001;
-console.log(port);
-
-app.listen(port, function(){
-  console.log("App listening port: " + port);
-});
-//
 module.exports = app;
